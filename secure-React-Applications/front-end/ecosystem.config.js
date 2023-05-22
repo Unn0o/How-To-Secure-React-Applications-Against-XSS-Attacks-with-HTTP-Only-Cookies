d6bb39c61431d743095c5a9724873f7ecc395f2a
Uuno Vaara <105356688+Unn0o@users.noreply.github.com>
2023-05-22 12:42:13 +0300

    Add files via upload

---
+++ secure-React-Applications/front-end/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'front-end',
+        cwd: '/app/jwt-storage-tutorial/front-end',
+        script: 'npm',
+        args: 'run start',
+        env: {
+          PORT: 3000
+        },
+      },
+    ],
+  };
